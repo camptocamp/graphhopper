2d5d7df6b931f6963545aa38c25faf469f469f23
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-10-31 11:40:01 +0100

    Add CI cron and docker hub deploy stub

--- c2c/dockerbuild.sh
+++ c2c/dockerbuild.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-TAG="$1"
-if [ -z "$TAG" ]
-then
-  echo 'You should provide a docker tag!'
-  exit 1
-fi
-
-docker build . --file c2c/Dockerfile -t $TAG
+docker build . --file c2c/Dockerfile -t camptocamp/graphhopper:latest
